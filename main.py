f3276801905a5cf2d07c18104ff9d51ddef46420
henriquecuryboaro <hcboaro@gmail.com.br>
2026-01-25 22:50:43 -0300

    acréscimo de novas funcionalidades

--- main.py
+++ main.py
@@ -24,11 +24,17 @@ data.iloc[:,4] = data.iloc[:,4].str.rstrip()
 ##Ajuste de textos em letras maiúsculas
 data.iloc[:,6],data.iloc[:,7],data.iloc[:,12] = data.iloc[:,6].str.capitalize(),data.iloc[:,7].str.capitalize(),data.iloc[:,12].str.capitalize()
 
-##tornar primeira letra de cada termo do endereço maiúscula e demais minúsculas
+##tornar primeira letra de cada termo do endereço maiúscula e demais minúsculas e fazer alterações em abreviações de identificações de logradouros
 data.iloc[:,2] = data.iloc[:,2].str.title()
+
+data.iloc[:,2] = data.iloc[:,2].str.replace('Avn', 'Av.', regex=False)
+data.iloc[:,2] = data.iloc[:,2].str.replace('Prc', 'Praça', regex=False)
+data.iloc[:,2] = data.iloc[:,2].str.replace('Lrg', 'Largo', regex=False)
+data.iloc[:,2] = data.iloc[:,2].str.replace('Trv', 'Travessa', regex=False)
 
 #Mudança de nomes de colunas:
 data = data.rename(columns={'ano_transação': 'ano', 'média_valor_imóvel':'media_valor', 'principal_transação_mercado':'transacao', 'total_transações':'obs','mês_transação':'mes'})
+
 
 #Manipulação de dados para obtenção de variáveis de interesse
 data['media_metro_quadrado'] = round((data.iloc[:,10]/data.iloc[:,9]),2)
@@ -37,7 +43,8 @@ data['media_metro_quadrado'] = round((data.iloc[:,10]/data.iloc[:,9]),2)
 def variacao_metro_bairro(bairro, tipologia):
     data_metro_quadrado = data[(data['bairro'] == bairro) & (data['principais_tipologias'] == tipologia)].groupby(['ano']).agg(media_metro_quadrado_max=('media_metro_quadrado', 'max'),
                                                             media_metro_quadrado_min=('media_metro_quadrado', 'min'),
-                                                            media_metro_quadrado_mean=('media_metro_quadrado', 'mean')).reset_index()
+                                                            media_metro_quadrado_mean=('media_metro_quadrado', 'mean'),
+                                                            transacoes=('obs','count')).reset_index()
 
     data_metro_quadrado.iloc[:,3] = round(data_metro_quadrado.iloc[:,3],2)
 
@@ -69,6 +76,17 @@ def ranking_bairro(tipologia,inicio,fim):
         .head(10)
     )
 
+
+@st.cache_data
+def variacao_metro_logradouro(logradouro, tipologia):
+    data_metro_quadrado_lgd = data[(data['logradouro'] == logradouro) & (data['principais_tipologias'] == tipologia)].groupby(['ano']).agg(media_metro_quadrado_max=('media_metro_quadrado', 'max'),
+                                                            media_metro_quadrado_min=('media_metro_quadrado', 'min'),
+                                                            media_metro_quadrado_mean=('media_metro_quadrado', 'mean'),
+                                                            transacoes=('obs','count')).reset_index()
+
+    data_metro_quadrado_lgd.iloc[:,3] = round(data_metro_quadrado_lgd.iloc[:,3],2)
+
+    return data_metro_quadrado_lgd
 
 def main():
     st.write('# Painel de informações do mercado imobiliário na cidade do Rio de Janeiro')
@@ -133,29 +151,30 @@ def main():
                     fig.update_layout(
                         title_x=0.5,           # Define a posição X como 0.5 (centro)
                         title_xanchor='center', # Garante que o centro do título fique no ponto 0.5
-                        title_font_size=24,
+                        title_font_size=18,
                         yaxis=dict(title='Valor médio do metro quadrado por transação (R$/m²)'),
                         xaxis=dict(title='Ano')
 
 
                     )
-                    fig.update_traces(marker=dict(size=12))
+                    fig.update_traces(marker=dict(size=16))
                     st.plotly_chart(fig)
 
                     data_metro_quadrado['variacao_anual'] = '-'
                     for i in range(1,len(data_metro_quadrado)):
-                        data_metro_quadrado.iloc[i,4] = round(100*((data_metro_quadrado.iloc[i,3] - data_metro_quadrado.iloc[i-1,3])/data_metro_quadrado.iloc[i-1,3]),2)
+                        data_metro_quadrado.iloc[i,5] = round(100*((data_metro_quadrado.iloc[i,4] - data_metro_quadrado.iloc[i-1,4])/data_metro_quadrado.iloc[i-1,4]),2)
 
                     data_metro_quadrado = data_metro_quadrado.rename(columns={'ano': 'Ano', 
                                                                         'media_metro_quadrado_max':'Maior valor de m² (R$/m²)', 
                                                                         'media_metro_quadrado_min':'Menor valor de m² (R$/m²)', 
                                                                         'media_metro_quadrado_mean':'Valor médio de m² (R$/m²)',
+                                                                        'transacoes':'Transações realizadas',
                                                                         'variacao_anual':'Variação anual (%)'
                                                                         })
 
                     ultima_obs = len(data_metro_quadrado)-1
                     variacao = round(100*((data_metro_quadrado.iloc[ultima_obs,3] - data_metro_quadrado.iloc[0,3])/data_metro_quadrado.iloc[0,3]),2)
-                    st.write(f'### Variação total observada no período para imóveis no bairo para a tipologia escolhida: {variacao}%')
+                    st.write(f'### Variação total observada no período para imóveis no bairro para a tipologia escolhida: {variacao}%')
                     st.dataframe(data_metro_quadrado, hide_index=True)
                 except ValueError:
                     st.write('### Não há resultados contemplados por esta busca')
@@ -211,9 +230,17 @@ def main():
 
             try:
                 inflacao_acumulada = ipca_acumulado(inicio,fim)
-                st.write(f'# IPCA acumulado entre {inicio} e {fim}: {round(100*(inflacao_acumulada),2)}%')
+                st.write(f'### IPCA acumulado entre {inicio} e {fim}: {round(100*(inflacao_acumulada),2)}%')
             except KeyError:
                 st.write('### Selecione um intervalo de datas para verificar indicadores financeiros. Caso nenhum valor de IPCA acumulado seja exibido, não há dados históricos para este período')
+                st.write(' ')
+                st.write('### Selecione abaixo um bairro e endereço para acompanhar a evolução de valores de transações neste local')
+                bairro_lgd = st.selectbox('Escolha o bairro',sorted(data['bairro'].unique()), index=None, placeholder='Bairros', key=f'bairro_lgd')
+                lgd_bairro_escolhido = data[data['bairro'] == bairro_lgd]
+                lgd_escolhido = st.selectbox('Escolha o logradouro',sorted(lgd_bairro_escolhido['logradouro'].unique()), index=None, placeholder='Logradouros', key=f'lgd_escolhido')
+                tipologia_lgd = st.selectbox('Escolha a tipologia',sorted(lgd_bairro_escolhido['principais_tipologias'].unique()), index=None, placeholder='Tipologia', key=f'tipo_escolhida')
+                data_logradouro = variacao_metro_logradouro(lgd_escolhido, tipologia_lgd)
+                st.dataframe(data_logradouro, hide_index=True)
 
         with con4:
             tipologia_var = st.selectbox('Escolha a tipologia',sorted(data['principais_tipologias'].unique()), index=None, placeholder='Tipologia', key=f'tipo_var')
